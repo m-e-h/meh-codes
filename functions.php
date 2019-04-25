86b9f0ff986362c6d525bc575ff0ceea65ee088d
m-e-h <info@martyhelmick.com>
2019-04-25 17:24:33 -0400

    enqueue style

--- functions.php
+++ functions.php
@@ -8,3 +8,19 @@
  * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0-or-later
  * @link      https://github.com/m-e-h/meh.codes
  */
+
+add_action( 'wp_enqueue_scripts', 'meh_scripts' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function meh_scripts() {
+	wp_enqueue_style(
+		'meh-style',
+		get_theme_file_uri( 'style.css' ),
+		[
+			'forsite-style',
+		],
+		wp_get_theme()->get( 'Version' )
+	);
+}
